2c0d40c338db59951784e0b6a6574140bd571655
Eric Masi <eric@gitmasi.com>
2019-02-24 13:17:10 -0500

    submit to https

--- process.php
+++ process.php
@@ -11,5 +11,5 @@
     //send email
     mail($admin_email, "New Form Submission", $message . ' - ' . $phone, "From:" . $email);
     
-    header('Location: http://www.gitmasi.com/success.html');
+    header('Location: https://www.gitmasi.com/success.html');
   }
